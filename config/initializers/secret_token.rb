559f4b1508e8bd1ec758f66ad28cd4a2bf90d66b
Sean Fisher <sean@Sprite.local>
2011-11-21 14:31:32 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dno::Application.config.secret_token = 'fc5707ab14055a6d9e330bad403aacf72bf2915923a8b51597221ba10b9ef2eb641cb289dc780c9ff4c88569b65fb3d578b5823e49eebbd5dc697b1df90e3c04'
